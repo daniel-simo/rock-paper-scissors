5c7697e02870737c63aba4208693101f76ab50c2
daniel-simo <dannyy.simo@hotmail.co.uk>
2022-08-14 16:32:03 +0100

    all functions wrote out mius code, player choice code complete so far, moving onto playing a round and how to console will check the winner.

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
+const choices = ['rock', 'paper', 'scissors'];
 
-const choice = ('rock', 'paper', 'scissors')
+function game(){
+    playRound();
 
-function getComputerChoice () {
-   return choice[Math.floor(Math.random() * choice.length)]
 }
+
+function playRound(){
+    const playerSelcetion = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+}
+
+function getPlayerChoice () {
+   let input = prompt('type rock, paper or scissors');
+   while(input == null){
+    input = prompt('Type Rock, Paper or Scissors')
+   }
+   //this makes it so if user presses cancel the code will not break.
+   input = input.toLowerCase();
+   let check = validateInput(input);
+   while (check == false){
+   input = prompt('Type Rock, Paper or Scissors, but caps does not matter');
+   while(input == null){
+    input = prompt('Type Rock, Paper or Scissors')
+   };
+   //if imput is incorrect gives a loop until the input is correct
+   input = input.toLowerCase();
+   check = validateInput(input);
+   }
+   //console.log(input)
+}
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+function validateInput(choice){
+    return choices.includes(choice);
+     //checks if input is correct
+}
+    game()
